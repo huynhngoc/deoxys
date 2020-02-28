390734cb5661622970b676963e6ecdd27eed8f9f
Ngoc Huynh <huynhbaongoc0412@gmail.com>
2020-02-28 20:59:56 +0100

    Added comet and tensorboard examples

--- examples/MNIST-experiment.py
+++ examples/MNIST-experiment.py
@@ -1,9 +1,22 @@
+"""
+To use comet.ml, uncomment this line
+"""
+###############################################
+# from comet_ml import Experiment as CometEx
+###############################################
 from deoxys.experiment import Experiment
 from deoxys.utils import read_file
 
-
 if __name__ == '__main__':
-
+    """
+    To use comet.ml, uncomment this line
+    """
+    ######################################################################
+    # # Create an experiment with your api key
+    # experiment = CometEx(api_key="YOUR_API_KEY",
+    #                      project_name="YOUR_PROJECT_NAME",
+    #                      workspace="YOUR_WORKSPACE")
+    ######################################################################
     config = read_file('examples/json/sequential-config.json')
 
     Experiment(log_base_path='../../mnist/logs').from_full_config(
