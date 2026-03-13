e73f74f16dd6c254484b2263b816b2e694a045e9
Bao Ngoc Huynh <huynhngoc@users.noreply.github.com>
2026-03-13 10:55:29 +0100

    Change import of OrderedDict to collections

--- src/deoxys/database/dbclient.py
+++ src/deoxys/database/dbclient.py
@@ -7,7 +7,7 @@ __email__ = "ngoc.huynh.bao@nmbu.no"
 from ..utils import Singleton
 from pymongo import MongoClient
 from bson.objectid import ObjectId
-from collections.abc import OrderedDict
+from collections import OrderedDict
 from datetime import datetime
 from time import time
 import json
